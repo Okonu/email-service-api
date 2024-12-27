e6442f6d17a55885a12a394e6571c3c471f29708
okonu <ianyakundi015@gmail.com>
2024-12-27 04:12:42 +0300

    add frontedn url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://okonu.vercel.app',
     methods: ['POST', 'GET'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
